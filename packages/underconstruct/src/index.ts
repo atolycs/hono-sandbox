606ec61e27440052f651ba2d8f63a658069ddb18
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-11-20 13:55:12 +0900

    feat: code update middleware

--- packages/underconstruct/src/index.ts
+++ packages/underconstruct/src/index.ts
@@ -1,11 +1,15 @@
 import { createMiddleware } from "hono/factory"
 
-export const underconstruction = (message: string = 'Hello!') => {
+export const underconstruction = () => {
     return createMiddleware(async (c, next) => {
         // @ts-ignore
         if (!import.meta.env.PROD) {
             console.log(`[${c.req.method}] Middleware Start URL: ${c.req.url}`)
         }
+        console.log(c?.req.url.includes("localhost"))
+        console.log(c?.req.url.match(/[^?]+.[^?]+.pages.dev/))
+        c.set("underconstruct", c?.req.url.includes("localhost"))
+
         await next()
 
         // @ts-ignore
