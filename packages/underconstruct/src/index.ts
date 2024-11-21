f2b91d123e08e489dfb5407b5da418df4f88202c
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-11-21 11:33:19 +0900

    feat: underconstruction middleware

--- packages/underconstruct/src/index.ts
+++ packages/underconstruct/src/index.ts
@@ -3,17 +3,18 @@ import { logger } from "hono/logger"
 
 export const UnderConstruction = () => {
     return createMiddleware(async (c, next) => {
+        const regexp_detect = new RegExp(/[\.]+.*\.pages\.dev/)
         // @ts-ignore
         if (!import.meta.env.PROD) {
             console.log(`[${c.req.method}] Middleware Start URL: ${c.req.url}`)
         }
         console.log(c?.req.url.includes("localhost"))
-        console.log(c?.req.url.match(/[^?]+.[^?]+.pages.dev/))
+        console.log(c?.req.url.match(regexp_detect))
         // @ts-ignore
         if ( 
             // @ts-ignore
             import.meta.env.PROD && 
-            (c?.req.url.includes("localhost")) || c?.req.url.match(/[^?]+.[^?]+.pages.dev/)
+            (c?.req.url.includes("localhost") || c?.req.url.match(regexp_detect))
         ) {
             c.set("underconstruct", true)
             c.header("X-UnderConstruction", "true")
@@ -21,7 +22,6 @@ export const UnderConstruction = () => {
         
         // @ts-ignore
         if (import.meta.env.DEV) {
-            console.info(import.meta.env.DEV)
             c.set("underconstruct", true)
         }
         await next()
